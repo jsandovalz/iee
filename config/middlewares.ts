8b2410c46686cc2b1c0981980e56201fe2012962
Jose Sandoval <jsandovalz@gmail.com>
2026-05-25 21:12:57 -0400

    migration from strapi cloud to railway

---
+++ config/middlewares.ts
@@ -0,0 +1,22 @@
+import type { Core } from '@strapi/strapi';
+
+const config: Core.Config.Middlewares = [
+  'strapi::logger',
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://iglesiaesperanzaenel.com"],
+    },
+  },
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
+
+export default config;
